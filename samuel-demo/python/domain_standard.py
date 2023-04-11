3243a2927cae6ea9bb5b0259481594c77a733ea9
Samuelll <41465278+ljp521@users.noreply.github.com>
2023-04-12 01:37:51 +0800

    Delete samuel-demo/python directory

--- samuel-demo/python/domain_standard.py
+++
@@ -1,9 +0,0 @@
-"""
-在url链接中提取出域名信息
-"""
-from tld import get_tld
-
-domain = "www.test.com"
-result = get_tld(domain, as_object=True, fix_protocol=True)
-domain = result.fld # .fld全域名 .subdomain域名前缀 .tld域名内容 .domain域名后缀
-print(domain)
