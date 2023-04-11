5a0571410b48d88bedfb07172864b8b7562f6e94
Samuelll <41465278+ljp521@users.noreply.github.com>
2023-04-12 02:08:59 +0800

    Update pack.py

--- python/gain_computer_information_for_win/pack.py
+++ python/gain_computer_information_for_win/pack.py
@@ -6,8 +6,8 @@ from PyInstaller.__main__ import run
 if __name__ == '__main__':
     opts = ['-F',
             '-w',
-            '--paths=D:/demo',
-            '--paths=D:/demo/ui',
+            '--paths=D:/demo', # 请自行修改路径
+            '--paths=D:/demo/ui',# 请自行修改路径
             '--noupx',
             '--clean',
             'init.py']
