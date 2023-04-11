d82818efa00dc16b3f72cb606bf3d1bb4d199828
Samuelll <41465278+ljp521@users.noreply.github.com>
2023-04-12 02:08:00 +0800

    Update pack.py

--- python/gain_computer_information_for_win/pack.py
+++ python/gain_computer_information_for_win/pack.py
@@ -6,9 +6,8 @@ from PyInstaller.__main__ import run
 if __name__ == '__main__':
     opts = ['-F',
             '-w',
-            '--paths=D:/py_workspace/test_demo',
-            '--paths=D:/py_workspace/test_demo/ui',
-            '--icon=D:/py_workspace/pims/stock_sync_tools/icon.ico',
+            '--paths=D:/demo',
+            '--paths=D:/demo/ui',
             '--noupx',
             '--clean',
             'init.py']
